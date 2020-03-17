a4a7819cf465482c5bedf0d88743eb4659300fae
AlexHuliai <58236141+AlexHuliai@users.noreply.github.com>
2020-03-17 15:05:04 -0500

    little update on Kafka

---
+++ Oleksandr/src/main/java/com/github/oleksandr/kafka/TwitterProducer.java
@@ -0,0 +1,21 @@
+package com.github.oleksandr.kafka;
+
+public class TwitterProducer {
+    public TwitterProducer(){
+
+    }
+    public static void main(String[] args) {
+    new TwitterProducer().run();
+
+
+
+
+    public void run(){
+
+        }
+
+        public void createTw
+    }
+
+
+}
